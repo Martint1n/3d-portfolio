79af1a2c5dbe7896db8ef1eb741c3360455b9844
guilbert <guilbertmartin75@gmail.com>
2024-05-14 01:23:44 +0200

    first 3d project, add .glb file, add three, fiber and spring, add tailwind. 3dModel should work

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,41 @@
-import styles from '../styles/Home.module.css';
+import { Suspense, useState, useEffect } from 'react';
+import { Canvas } from '@react-three/fiber';
+import Loader from '../components/Loader';
+import Tokyo from '../models/Tokyo';
 
 function Home() {
+  const [screenScale, setScreenScale] = useState(null);
+  const [screenPosition, setScreenPostion] = useState([0, -6.5, -43]);
+  const [rotation, setRotation] = useState([0.1, 4.7, 0]);
+
+
+  useEffect(() => {
+    (function  () { // adjustTokyoForScreenSize 
+      if(window.innerWidth < 768) {
+        setScreenScale([0.9, 0.9, 0.9]);
+      }else {
+        setScreenScale([1, 1, 1]);
+      }
+      return [screenScale, screenPosition, rotation]
+    })()
+  }, [])
+
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+    <div className='w-full h-screen relative'>
+      <Canvas 
+        className='w-full h-screen bg-transparent'
+        camera={{near: 0.1, far: 1000,}} // les éléments entre 0.1 et 1000 seront affichés
+      >
+      <Suspense fallback={<Loader />}> //loading screen while waiting for 3D model
+
+      </Suspense>
+      <Tokyo 
+        scale= {screenScale}
+        position={screenPosition}
+        rotation={rotation}
+
+      />
+      </Canvas>
     </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -4,6 +4,29 @@ const nextConfig = {
   images: {
     domains: [],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/:path*',
+      },
+    ];
+  },
+  webpack: (config, { isServer }) => {
+    // Ajouter une règle de chargement pour les fichiers .glb
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/', // Chemin public de sortie
+          outputPath: 'static/', // Dossier de sortie dans .next/static/
+        },
+      },
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
