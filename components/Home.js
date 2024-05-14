a6dd26f1584b7d859130aa1b288af41e259a3075
guilbert <guilbertmartin75@gmail.com>
2024-05-14 10:06:29 +0200

    2 spotlights added

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,9 @@
-import { Suspense, useState, useEffect } from 'react';
+import { Suspense, useState, useEffect, useMemo } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader';
 import Tokyo from '../models/Tokyo';
+import { OrbitControls } from '@react-three/drei';
+import { SpotLight } from 'three';
 
 function Home() {
   const [screenScale, setScreenScale] = useState(null);
@@ -10,30 +12,53 @@ function Home() {
 
 
   useEffect(() => {
-    (function  () { // adjustTokyoForScreenSize 
-      if(window.innerWidth < 768) {
+    function adjustTokyoForScreenSize() {
+      if (window.innerWidth < 768) {
         setScreenScale([0.9, 0.9, 0.9]);
-      }else {
+      } else {
         setScreenScale([1, 1, 1]);
       }
-      return [screenScale, screenPosition, rotation]
-    })()
+    }
+
+    adjustTokyoForScreenSize();
   }, [])
+  const spotlight = useMemo(() => new SpotLight('#fff'), []);
+  const spotlightTowardUser = useMemo(() => new SpotLight('#fff'), []); // Créer une instance pour le deuxième projecteur
 
   return (
     <div className='w-full h-screen relative'>
       <Canvas 
-        className='w-full h-screen bg-transparent'
+        className='w-full h-screen bg-transparent absolute'
         camera={{near: 0.1, far: 1000,}} // les éléments entre 0.1 et 1000 seront affichés
       >
       <Suspense fallback={<Loader />}> //loading screen while waiting for 3D model
-        <directionalLight position={[1, 1, 1]} intensity={2}/> 
+
+        <group>
+          <primitive
+            object={spotlight}
+            position={[0, 5, -70]}
+            angle={4.1}
+            intensity={10000}
+            penumbra={0.5}
+          />
+          <primitive object={spotlight.target} position={[0, 0, -120]} />
+        </group>
+        <group>
+          <primitive
+            object={spotlightTowardUser}
+            position={[0, 5, -70]}
+            angle={Math.PI/2}
+            intensity={10000}
+            penumbra={0.1}
+          />
+          <primitive object={spotlightTowardUser.target} position={[0, 0, 120]} />
+        </group>
+        <OrbitControls />
       </Suspense>
       <Tokyo 
         scale= {screenScale}
         position={screenPosition}
         rotation={rotation}
-
       />
       </Canvas>
     </div>
