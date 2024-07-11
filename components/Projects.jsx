9cd7ff4987be2d3ebb69dc4b208402785d50304c
guilbert <guilbertmartin75@gmail.com>
2024-07-11 02:03:50 +0200

    change link color to yellow and add tournoi website link

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -14,8 +14,8 @@ function Projects () {
       <p className='text-whiteBlue text-3xl'>RenoSkem</p>
       <p className=''>{t('projects.renoskem.introduction')}</p>
       <div className='flex flex-col'>
-        <a className='text-whiteBlue pb-2 underline' href='https://github.com/craftByElla/RenoSkem_Frontend' target="_blank">{t('projects.renoskem.linkGithubFront')}</a>
-        <a className='text-whiteBlue underline' href='https://github.com/craftByElla/RenoSkem_Backend' target="_blank">{t('projects.renoskem.linkGithubFront')}</a>
+        <a className='text-yellow pb-2 underline' href='https://github.com/craftByElla/RenoSkem_Frontend' target="_blank">{t('projects.renoskem.linkGithubFront')}</a>
+        <a className='text-yellow underline' href='https://github.com/craftByElla/RenoSkem_Backend' target="_blank">{t('projects.renoskem.linkGithubFront')}</a>
       </div>
       
       <p className='text-yellow underline' onClick={onOpen}>{t('projects.renoskem.video')}</p>
@@ -35,9 +35,9 @@ function Projects () {
     <p>{t('projects.tournoi.introduction')}</p>
     <p>{t('projects.tournoi.design')}</p>
     <div className='flex flex-col'>
-      <a className='text-whiteBlue pb-2 underline' target='_blank' href=''>{t('projects.tournoi.websiteLink')}</a>
-      <a className='text-whiteBlue pb-2 underline' href='https://github.com/Martint1n/HousesTournament' target="_blank">{t('projects.tournoi.linkGithubFront')}</a>
-      <a className='text-whiteBlue underline' href='https://github.com/Martint1n/HouseTournamentBackend' target="_blank">{t('projects.tournoi.linkGithubBack')}</a>
+      <a className='text-yellow pb-2 underline' target='_blank' href='https://houses-tournament.vercel.app'>{t('projects.tournoi.websiteLink')}</a>
+      <a className='text-yellow pb-2 underline' href='https://github.com/Martint1n/HousesTournament' target="_blank">{t('projects.tournoi.linkGithubFront')}</a>
+      <a className='text-yellow underline' href='https://github.com/Martint1n/HouseTournamentBackend' target="_blank">{t('projects.tournoi.linkGithubBack')}</a>
     </div>
   </div>
 
@@ -46,8 +46,8 @@ const lunetoile = <div className='w-[60vw] h-full text-center flex flex-col just
   <p>{t('projects.lunetoile.introduction')}</p>
   <p>{t('projects.lunetoile.websiteLink')}</p>
   <div className='flex flex-col'>
-    <a className='text-whiteBlue pb-2 underline' href='https://github.com/Martint1n/lunetoile-frontend' target="_blank">{t('projects.lunetoile.linkGithubFront')}</a>
-    <a className='text-whiteBlue underline' href='https://github.com/Martint1n/lunetoile-backend' target="_blank">{t('projects.lunetoile.linkGithubBack')}</a>
+    <a className='text-yellow pb-2 underline' href='https://github.com/Martint1n/lunetoile-frontend' target="_blank">{t('projects.lunetoile.linkGithubFront')}</a>
+    <a className='text-yellow underline' href='https://github.com/Martint1n/lunetoile-backend' target="_blank">{t('projects.lunetoile.linkGithubBack')}</a>
   </div>
 </div>
 
